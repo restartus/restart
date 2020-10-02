1700279265b35b5277954309a2a6e0b4d2a00d66
Lucas Hahn <lhahn1@me.com>
2020-10-02 08:26:45 -0700

    update des filtering

--- restart/__init__.py
+++ restart/__init__.py
@@ -1,7 +1,7 @@
 """Initialization script for restart package."""
 
 # Version
-__version__ = "2.6.5"
+__version__ = "2.6.6"
 
 # Public interface for the model
 from restart.data import Data
--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="restart",
-    version="2.6.5",
+    version="2.6.6",
     author="Restart Partners",
     author_email="lucas@restart.us",
     description="COVID-19 decision model tool",
